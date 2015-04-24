ad2918ade96ba657c99c92db6c6defd949b2714f
Alex Grant <alexmgrant@me.com>
2015-04-24 13:18:59 -0400

    changing _getOffset function to use getBoundingClientRect function

--- intro.js
+++ intro.js
@@ -1108,17 +1108,22 @@
     elementPosition.height = element.offsetHeight;
 
     //calculate element top and left
-    var _x = 0;
-    var _y = 0;
-    while (element && !isNaN(element.offsetLeft) && !isNaN(element.offsetTop)) {
-      _x += element.offsetLeft;
-      _y += element.offsetTop;
-      element = element.offsetParent;
-    }
-    //set top
-    elementPosition.top = _y;
-    //set left
-    elementPosition.left = _x;
+    //var _x = 0;
+    //var _y = 0;
+    //while (element && !isNaN(element.offsetLeft) && !isNaN(element.offsetTop)) {
+    //  _x += element.offsetLeft;
+    //  _y += element.offsetTop;
+    //  element = element.offsetParent;
+    //}
+    ////set top
+    //elementPosition.top = _y;
+    ////set left
+    //elementPosition.left = _x;
+
+    // changes to lib
+    var clientRect = element.getBoundingClientRect();
+    elementPosition.top = clientRect.top;
+    elementPosition.left = clientRect.left;
 
     return elementPosition;
   }
